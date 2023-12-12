26f07d9d8d1519152d427b2fc2e983cd3a7eaf13
kristine bermoy <kbermoy02@gmail.com>
2023-12-11 19:18:30 -0800

    implented the search city and state function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const form = document.querySelector('.formContainer')
 const userInput = document.querySelector('.nav__input')
 const clearInput = document.querySelector('.btn--clear')
-// const sliderContainer = document.querySelectorAll('chrome')
 const slider = document.getElementById('myInput')
 const sliderVal = document.querySelector('.sliderVal')
 const restaurantWrapper = document.querySelector('.resturants__row')
@@ -10,17 +9,7 @@ const restaurantWrapper = document.querySelector('.resturants__row')
     This block deals with the search input from the nav
 */
 
-function search() {
-    document.body.classList += ' btn--clear-on'
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const inputValue = userInput.value
-    userInput.value = ''
-    console.log(inputValue)
-})
+let locationUserInput = [`West%20Bloomfield` ,`mi`]
 
 
 // this allows the background purple color to change with the slider thumb
@@ -30,36 +19,33 @@ document.getElementById('myInput').oninput = function (){
     sliderVal.innerHTML = slider.value
 }
 
-// function for handling the slider values
-function customSlider() {
-    // const maxVal = slider.getAttribute('max')
-    // const val = (slider.value / maxVal) * 100 + '%'
-
-    // sliderVal.innerHTML = slider.value
+// getting data from api
+function url() {
+  return `https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/${locationUserInput[1]}/city/${locationUserInput[0]}/0`
 }
 
-// getting data from api
-const request = new Request('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/MI/city/West%20Bloomfield/0', {
-  method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '76e31ebb81msh011bc3129b5b491p1112d1jsn7644e0f23ae1',
-		'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
+function request() {
+  return new Request(url(), {
+    method: 'GET',
+	  headers: {
+		  'X-RapidAPI-Key': '76e31ebb81msh011bc3129b5b491p1112d1jsn7644e0f23ae1',
+		  'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
 	}
-})
+  })
+}
 
 async function rendorResturants() {
-
   try {
-    const data = await fetch(request)
-    const response = await data.json() // response if returning an object
+    let data = await fetch(request())
+    let response = await data.json() // response if returning an object
     
     if(data.status === 200) {
-      let responseHTML;
+      let responseHTML = ''
 
       for(let i = 0; i < response['restaurants'].length; i++) {
         responseHTML += restaurantsHTML(response['restaurants'][i])
       }
-
+      
       restaurantWrapper.innerHTML = responseHTML
 
     } else {
@@ -106,9 +92,30 @@ setTimeout(() => {
     rendorResturants()
 }, 1000)
 
+function search() {
+  document.body.classList += ' btn--clear-on'
+}
+
+function clearSearch () {
+  document.body.classList.remove('btn--clear-on')
+}
 
 // event listeners
-clearInput.addEventListener('click', () => {
-    document.body.classList.remove('btn--clear-on')
-    input.value = ''
+form.addEventListener('submit', e => {
+  document.body.classList += ' btn--clear-on'
+  e.preventDefault()
+  
+  let inputValue = userInput.value
+  userInput.value = ''
+
+  console.log(inputValue)
+  
+  locationUserInput = inputValue.split(', ')
+
+  console.log(locationUserInput)
+  console.log(url())
+  url()
+  rendorResturants()
 })
+
+clearInput.addEventListener('click', clearSearch)
